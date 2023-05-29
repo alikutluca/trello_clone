1037bf9aef0d7d1bb88e70a6bef95d1ad3e8e2a0
Ali Kutluca <alikutluca@yahoo.com>
2023-05-29 22:43:10 +0300

    fix: draggable components

---
+++ components/Column.tsx
@@ -0,0 +1,72 @@
+import { Draggable, Droppable } from "react-beautiful-dnd";
+
+type Props = {
+  id: TypedColumn;
+  todos: Todo[];
+  index: number;
+};
+
+const idToColumText: {
+  [key in TypedColumn]: string;
+} = {
+  todo: "To Do",
+  inprogress: "In Progress",
+  done: "Done",
+};
+
+function Column({ id, todos, index }: Props) {
+  return (
+    <Draggable draggableId={id} index={index}>
+      {(provided) => (
+        <div
+          {...provided.draggableProps}
+          {...provided.dragHandleProps}
+          ref={provided.innerRef}
+        >
+          {/* Render droppable todos in the column */}
+          <Droppable droppableId={index.toString()} type="card">
+            {(provided, snapshot) => (
+              <div
+                {...provided.droppableProps}
+                ref={provided.innerRef}
+                className={`pb-2 p-2 rounded-2xl shadow-sm ${
+                  snapshot.isDraggingOver ? "bg-green-200" : "bg-white/50"
+                }`}
+              >
+                <h2 className="flex justify-between font-bold text-xl p-2">
+                  {idToColumText[id]}
+                  <span className="text-gray-500 bg-gray-200 rounded-full px-2 py-1 text-sm font-normal">
+                    {todos.length}
+                  </span>
+                </h2>
+
+                <div className="space-y-2">
+                  {todos.map((todo, index) => (
+                    <Draggable
+                      key={todo.$id}
+                      draggableId={todo.$id}
+                      index={index}
+                    >
+                      {(provided) => (
+                        <TodoCard
+                          todo={todo}
+                          index={index}
+                          id={id}
+                          innerRef={provided.innerRef}
+                          draggableProps={provided.draggableProps}
+                          dragHandleProps={provided.dragHandleProps}
+                        />
+                      )}
+                    </Draggable>
+                  ))}
+                </div>
+              </div>
+            )}
+          </Droppable>
+        </div>
+      )}
+    </Draggable>
+  );
+}
+
+export default Column;
